63995a6d1426c8f107271b4a09c95a12ffa1142d
anderson agbonoga <andymaintain1@gmail.com>
2022-04-21 18:23:50 +0100

    git

--- 14aws_ec2_database.tf
+++ 14aws_ec2_database.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "demo_vpc_mysql" {
 	instance_type = "t2.micro"
 	key_name 	  = aws_key_pair.deployer.key_name #depends on file 13
 	subnet_id 	  = aws_subnet.demo_vpc_private_sg1[0].id #depends on file 4
-  user_data     = <<EOF
+    user_data     = <<EOF
 	  #!/bin/bash 
     sudo wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm 
     sudo yum localinstall mysql57-community-release-el7-11.noarch.rpm
--- variables.tf
+++ variables.tf
@@ -18,10 +18,10 @@
 #    default = "deployer-key"
 #  }
 
-# variable "aws_region" {
-#   description = "AWS region to launch servers."
-#   default     = "eu-west-1"
-# }
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "eu-west-1"
+}
 
 variable "aws_availability_zone" {
   description = "AWS Availability Zone to launch servers."
@@ -62,21 +62,21 @@ variable "aws_amis" {
   }
 }
 
-# variable "aws_access_key" {
-#   description = "AWS access key"
-#   type        = string
-# }
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
 
-# variable "aws_secret_key" {
-#   description = "AWS secret key"
-#   type        = string
-# }
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
 
 # variable "region" {
 #   description = "The aws region. https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html"
 #   type        = string
 #   default     = "eu-west-1"
-#}
+# }
 
 variable "availability_zones_count" {
   description = "The number of AZs."
